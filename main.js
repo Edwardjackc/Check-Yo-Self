99dd16defa98f2fe8fbeff65813252aa4c9853ae
Edward Cheatham <edwardjackc@gmail.com>
2019-04-13 11:17:04 -0600

    Add var and evlisteners

---
+++ main.js
@@ -0,0 +1,16 @@
+var titleInput = document.querySelector('');
+var itemInput = document.querySelector('');
+var makeList = document.querySelector('');
+var clearAll = document.querySelector('');
+var filterUrgency = document.querySelector('');
+
+
+titleInput.addEventListener('keydown',);
+itemInput.addEventListener('keydown',);
+makeList.addEventListener('click',);
+clearAll.addEventListener('click',);
+filterUrgency.addEventListener('click',);
+
+
+var globalArray = JSON.parse(localStorage.getItem()) || [];
+var tasksArray = []; 
